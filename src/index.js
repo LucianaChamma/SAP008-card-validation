7d73ce553960c64528e9ea8afa41c14bc61ede7c
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2022-07-28 10:20:41 -0300

    subindo pro github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import validator from './validator.js';
 
-console.log(validator);
+console.log(validator.isValid ("8956895654788978"));
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
-  // ...
+  isValid:function (cardNumber) {
+    console.log(cardNumber)
+  }
+  
 };
+
+
+
+// function maskify(creditCard) {
+// 	if (CreditCard.length < 6) return creditCard;
+// 	const last4Characters = creditCard.substr(-4);
+// 	const firstCharacter = creditCard.substr(0, 1);
+// 	const maskingCharacters = creditCard.substr(1, creditCard.length - 5).replace(/\d/g, '#');
+// 	return `${firstCharacter}${maskingCharacters}${last4Characters}`;
+// }
 
 export default validator;
