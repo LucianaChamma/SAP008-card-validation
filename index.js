bc821b2b7b832240f07180c35a4d2629bd7d4c12
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2022-08-09 23:47:36 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,105 @@
+import validator from "./validator.js";
+
+let runbtn = document.getElementById("btnCartao");
+// let finalResult = document.getElementById("outCard");
+runbtn.addEventListener("click", validatorCard);
+
+function validatorCard() {
+	let numCartao = document.getElementById("numCartao").value;
+	let result = validator.isValid(numCartao);
+	let cardMaskify = validator.maskify(numCartao);
+	console.log(result)
+
+	// Condicionais para exibição do resultado
+	if (numCartao === "") {
+		return alert("Digite um número");
+	}
+	else if (result == true) {
+		alert(cardMaskify + " CARTÃO VÁLIDO");
+	}
+	else {
+		alert(cardMaskify + " CARTÃO INVÁLIDO");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const cartao = document.querySelector('#cartao'),
+	  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+	 formulario = document.querySelector('#formulario-cartao');
+
+	  numCartao = document.querySelector("=numDigitado");
+	  nomCartao = document.querySelector("nomeCartao");
+
+	  mesValidade = document.querySelector('#cartao .mes');
+	  anoValidade = document.querySelector('#cartao .ano');
+	
+
+// * Btn de abrir formulario
+btnAbrirFormulario.addEventListener('click', () => {
+	btnAbrirFormulario.classList.toggle('active');
+	formulario.classList.toggle('active');
+});
+
+
+// * Input numero cartão
+formulario.inputNumCartao.addEventListener('keyup', (e) );
+	let valorInput = e.target.value;
+
+	formulario.inputNumero.value = valorInput
+	// Eliminar espaços em branco
+	.replace(/\s/g, '')
+	// Eliminar as letras
+	.replace(/\D/g, '')
+	// Colocar Espaço a cada quatro números 
+	.replace(/([0-9]{4})/g, '$1 ')
+	// Elimina o ultimo espaço
+	.trim();
+
+	numCartao.textContent = valorInput;
+
+	if(valorInput == ''){
+		numCartao.textContent = '#### #### #### ####';
+	}
+
+// * Input nome do cartão
+formulario.inputNome.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+
+	formulario.inputNome.value = valorInput.replace(/[0-9]/g, '');
+	nomeCartao.textContent = valorInput;
+
+	if(valorInput == ''){
+		nomeCartao.textContent = 'Luciana Chamma';
+	}
+
+});
+
+
+
+/*import validator from './validator.js';
+
+const cardInput = document.getElementById ("creditCard");
+const btn = document.getElementById ("validar");
+btn.addEventListener ("click", function (){
+
+    console.log ("click")
+})
+*/
+
+
+//console.log(validator.isValid ("8956895654788978"));
+
